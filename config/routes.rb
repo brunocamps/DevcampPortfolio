c0c5207a19dfff2c3811b3b63f7ba3ef7a429ecc
brunocamps <bruno.mcamps@outlook.com>
2020-07-25 10:03:54 -0300

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show' #mapping the data
   get 'about-me', to: 'pages#about' #pass anything you want as name (about-me)
   get 'contact', to: 'pages#contact'
 
