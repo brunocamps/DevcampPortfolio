13ecf316379cad38facadc66cd5025447ab71147
brunocamps <bruno.mcamps@outlook.com>
2020-07-26 11:07:07 -0300

    Work in progress - custom action

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
-    enum status: {draft: 0, published: 1}
+    enum status: {draft: 0, published: 1} #only spot 1 and 0
+    #0 is going to be mapped to draft.
+    #Whenever we create a new blog post, by default, it's going to be on draft mode until
+    #we change it to published and change the state
     extend FriendlyId 
     friendly_id :title, use: :slugged
     #changed it to title (title of the blog)
