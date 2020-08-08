3d8a578b2188638bd7e8cb5431e377627998412f
brunocamps <bruno.mcamps@outlook.com>
2020-08-08 07:53:34 -0300

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first #self says the specific name for the user we're talking about
+  end
+
+  def last_name
+    self.name.split.last
+
+  end
 end
